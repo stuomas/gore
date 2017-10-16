a8c40ad2a59ac5b91214fd2077e9ff114afda061
Tuomas Salokanto <tuomas.salokanto@gmail.com>
2017-10-16 14:06:33 +0300

    Adding parameters

--- gore.go
+++ gore.go
@@ -7,24 +7,35 @@ import (
 	"os/exec"
 )
 
+//func parseFlags
+//func goBuild
+//func runSCP
+//func runSSH
+
 func main() {
+	// TODO: Make separate functions
+
 	// Takes filename (without .go) as user input
 	flagFile := flag.String("f", "", "File to be compiled and executed at target.")
 	flag.Parse()
 
-	oss := "Linux"
-	arm := "7"
+	// TODO: Add as flags and with defaults read from configuration file (toml?)
+	osys := "linux"
+	armv := "7"
 	arch := "ARM"
+	targ := "rasp"
+	user := "pi"
+	tdir := "/home/pi/go/bin/"
 
 	// Start build process
 	buildArg := "/home/tuomas/go/src/github.com/stuomas/piserve/" + *flagFile + ".go"
 	cmdBuild := exec.Command("go", "build", buildArg)
 	cmdBuild.Env = append(os.Environ(),
-		"GOARM=7",
-		"GOOS=linux",
-		"GOARCH=arm",
+		"GOARM=" + armv,
+		"GOOS=" + osys,
+		"GOARCH=" + arch,
 	)
-	fmt.Printf("Cross-compiling for %s %s...", arch+arm, oss)
+	fmt.Printf("Cross-compiling for %s %s...", arch+armv, osys)
 	errBuild := cmdBuild.Run()
 	if errBuild != nil {
 		fmt.Printf("Failure!\n")
@@ -33,11 +44,9 @@ func main() {
 	}
 
 	// Start scp process
-	targetLoc := "pi@rasp"
-	targetDir := "/home/pi/go/bin/"
-	scpArgs := []string{*flagFile, targetLoc + ":" + targetDir}
+	scpArgs := []string{*flagFile, user + "@" + targ + ":" + tdir}
 	cmdScp := exec.Command("scp", scpArgs...)
-	fmt.Printf("Copying binary to target %s...", targetDir)
+	fmt.Printf("Copying binary to target %s...", tdir)
 	errScp := cmdScp.Run()
 	if errScp != nil {
 		fmt.Printf("Failure!\n")
@@ -48,7 +57,7 @@ func main() {
 	// Execute via SSH at target
 	sshArgs := []string{"-t", "pi@rasp", "'./main'"}
 	cmdSSH := exec.Command("ssh", sshArgs...)
-	fmt.Printf("Running freshly built binary at target %s...", targetLoc)
+	fmt.Printf("Running freshly built binary at target...")
 	errSSH := cmdSSH.Run()
 	if errSSH != nil {
 		fmt.Printf("Failure!\n")

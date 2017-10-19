beee0731833baf893c7b7e1bc12d1d1a6dbbe00b
Tuomas Salokanto <tuomas.salokanto@gmail.com>
2017-10-19 15:28:28 +0300

    Add config file and parser, improve help text

--- README.md
+++ README.md
@@ -1,29 +1,27 @@
 # gore
 Go remote run, an all-in-one Linux command for cross-compilation and execution of Go programs on a remote machine.
 
-Born out of an engineer's laziness, `gore` seeks to combine three commands, `go build`, `scp`, and `ssh` with all their necessary arguments and environment variables into one goreous tool trying to mimic the `go run` command while targeting a remote machine. Myself I am using `gore` to code on my laptop and running the binary on a Raspberry Pi.
+Born out of an engineer's laziness, `gore` seeks to combine three commands, `go build`, `scp`, and `ssh` with all their necessary arguments and environment variables into one goreous tool trying to mimic the `go run` command while targeting a remote machine. Unlike `go run`, `gore run` can be run directly from your working directory without specifying a source file.
 
 ## Installation
 
 `go get github.com/stuomas/gore`
 
 ## Usage
-Set the parameters in the configuration file and command `gore <possible parameters> /path/to/source.go` or from inside the working directory just command `gore`
+Set the parameters in the configuration file and command `gore run <optional parameters> <optional file path>`
 
-Optionally, set the parameters as arguments:
+If you want to experiment, you can also set the parameters as command arguments:
 ```
-  -help
-    Get help.
   -arch
     Target architecture.
+  -arm
+    ARM version.
   -dir
     Target directory.
-  -os 
-    Target operating system.
   -host
     Target IP or hostname.
   -user
     Username at target.
-  -arm
-    ARM version.
+  -os 
+    Target operating system.
 ```
--- gore.go
+++ gore.go
@@ -7,7 +7,17 @@ import (
 	"os/exec"
 	"path/filepath"
 	"strings"
+	"github.com/BurntSushi/toml"
 )
+
+type Configuration struct {
+	GOOS string
+	GOARCH string
+	GOARM string
+	USERNAME string
+	HOSTNAME string
+	DIRECTORY string
+}
 
 //Build the input source file
 func runGoBuild(args []string, osys string, arch string, armv string) {
@@ -18,8 +28,7 @@ func runGoBuild(args []string, osys string, arch string, armv string) {
 		"GOARCH="+arch,
 	)
 	fmt.Printf("Cross-compiling for %s %s...", arch+armv, osys)
-	stdoutStderr, err := cmdBuild.CombinedOutput()
-	if err != nil {
+	if stdoutStderr, err := cmdBuild.CombinedOutput(); err != nil {
 		fmt.Printf("\x1b[31;1mFailed!\x1b[0m\n \u2937 %s", stdoutStderr)
 		os.Exit(1)
 	} else {
@@ -29,10 +38,9 @@ func runGoBuild(args []string, osys string, arch string, armv string) {
 
 //Copy binary to target with scp
 func runSCP(args []string) {
-	cmdScp := exec.Command("scp", args...)
+	cmdSCP := exec.Command("scp", args...)
 	fmt.Printf("Copying binary to target...")
-	stdoutStderr, err := cmdScp.CombinedOutput()
-	if err != nil {
+	if stdoutStderr, err := cmdSCP.CombinedOutput(); err != nil {
 		fmt.Printf("\x1b[31;1mFailed!\x1b[0m\n \u2937 %s", stdoutStderr)
 		os.Exit(1)
 	} else {
@@ -42,55 +50,65 @@ func runSCP(args []string) {
 
 //Execute binary at target via SSH
 func runSSH(args []string) {
-	//TODO: password prompt not working, only key-based authentication works
-	cmdSSH := exec.Command("ssh", args...)
-	fmt.Printf("Running freshly built binary at target...")
-	stdoutStderr, err := cmdSSH.CombinedOutput()
-	if err != nil {
+	cmdSSH := exec.Command("ssh", args...) //TODO: password prompt not working, only key-based authentication works
+	fmt.Printf("Running binary at target... Press ^C to stop.")
+	if stdoutStderr, err := cmdSSH.CombinedOutput(); err != nil {
 		fmt.Printf("\x1b[31;1mFailed!\x1b[0m\n \u2937 %s", stdoutStderr)
 		os.Exit(1)
-	} else {
-		fmt.Printf("\x1b[32;1mSuccess!\x1b[0m\n") //TODO: Not executed
 	}
 }
 
-func main() {
-	//TODO: Defaults read from configuration file (toml?)
-	osysDefault := "linux"
-	armvDefault := "7"
-	archDefault := "arm"
-	targDefault := "rasp"
-	userDefault := "pi"
-	tdirDefault := "/home/pi/go/bin/"
+//Read configuration file for environment variables and connection parameters
+func readConfig() Configuration {
+	var config Configuration
+	if _, err := toml.DecodeFile("config.toml", &config); err != nil {
+		fmt.Println("Error reading configuration file. Please check that config.toml exists and syntax is correct.")
+		os.Exit(1)
+	} 
+	return config
+}
 
-	//Flags set by user
-	flagOsys := flag.String("os", osysDefault, "Target operating system.")
-	flagArch := flag.String("arch", archDefault, "Target architecture.")
-	flagArmv := flag.String("arm", armvDefault, "ARM version.")
-	flagTarg := flag.String("host", targDefault, "Target IP or hostname.")
-	flagUser := flag.String("user", userDefault, "Username at target.")
-	flagTdir := flag.String("dir", tdirDefault, "Target directory.")
+func main() {
+	config := readConfig()
+	flagOsys := flag.String("os", config.GOOS, "Target operating system.")
+	flagArch := flag.String("arch", config.GOARCH, "Target architecture.")
+	flagArmv := flag.String("arm", config.GOARM, "ARM version.")
+	flagTarg := flag.String("host", config.HOSTNAME, "Target IP or hostname.")
+	flagUser := flag.String("user", config.USERNAME, "Username at target.")
+	flagTdir := flag.String("dir", config.DIRECTORY, "Target directory.")
 	flag.Parse()
 
 	var sourcePath string
 	var localPath string
 	var packageName string
-	var buildArgs []string
+	buildArgs := []string{"build"}
 
-	if len(flag.Args()) > 0 {
-		sourcePath = flag.Arg(0)
+	if flag.Arg(0) != "run" {
+		fmt.Println("\nSyntax: gore run <optional parameters> <optional path>\n\nAvailable parameters (set preferably in config.toml):\n")
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	switch len(flag.Args()) { //Non-flag arguments, starts counting from AFTER last flag
+	case 1:
+		var err error
+		if sourcePath, err = filepath.Abs(filepath.Dir(os.Args[0])); err != nil {
+			fmt.Println("Could not read path.")
+			os.Exit(1)
+		}
+		packageName = filepath.Base(sourcePath)
+	case 2:
+		sourcePath = flag.Arg(1)
 		packageName = strings.TrimSuffix(filepath.Base(sourcePath), ".go")
-		buildArgs = []string{"build", sourcePath}
+		buildArgs = append(buildArgs, packageName)
 		localPath = strings.TrimSuffix(sourcePath, filepath.Base(sourcePath))
 		if !strings.HasSuffix(sourcePath, ".go") {
 			fmt.Println("Absolute path needs to target a source file.")
 			os.Exit(1)
 		}
-	} else {
-		localPath = ""
-		sourcePath, _ = filepath.Abs(filepath.Dir(os.Args[0]))
-		packageName = filepath.Base(sourcePath)
-		buildArgs = []string{"build"}
+	default:
+		fmt.Println("Check arguments.")
+		os.Exit(1)
 	}
 
 	runGoBuild(buildArgs, *flagOsys, *flagArch, *flagArmv)
